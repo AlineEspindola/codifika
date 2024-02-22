4381335d992b7f825e7015a465a181fed6e672c1
AlineEspindola <aline.ae@aluno.ifsc.edu.br>
2024-02-22 10:37:14 -0300

    feat: Adiciona estrutura HTML

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import './reset.css';
 import './App.css';
+import Form from './components/Form';
+import Button from './components/Button';
+import logo from './assets/logo.png';
 class App extends Component {
 
   render() {
     return(
       <div className="container">
-        
+        <div className='container__content'>
+          <figure>
+            <img src={logo} alt="Logo da Codifika" />
+          </figure>
+          <div className='forms'>
+            <form>
+              <Form/>
+              <Button/>
+            </form>
+            <form>
+              <Form/>
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/components/Form/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Select from '../Select';
+import './Form.css';
+class Form extends Component {
+  render() {
+    return(
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default Form;
