3eaa0b0df3874e76e5adc681626b7588451a4599
AlineEspindola <aline.ae@aluno.ifsc.edu.br>
2024-02-23 00:01:59 -0300

    feat: Adiciona função de pegar texto do textarea

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -2,6 +2,20 @@ import React, { Component } from 'react';
 import Select from '../Select';
 import './Form.css';
 class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ''
+    }
+
+    this.changeText = this.changeText.bind(this);
+  }
+
+  changeText(e) {
+    let enteredValue = e.target.value;
+    this.setState({text: enteredValue});
+  }
+
   render() {
     const { labelText } = this.props;
     const { placeholderText } = this.props;
@@ -12,7 +26,7 @@ class Form extends Component {
           <label>{labelText}</label>
           <Select/>
         </div>
-        <textarea rows="4" cols="50" placeholder={placeholderText}></textarea>
+        <textarea rows="4" cols="50" value={this.state.textarea} onChange={this.changeText} placeholder={placeholderText}></textarea>
       </div>
     )
   }
