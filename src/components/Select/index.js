cc7e4d5d45bc8db25579dbc34ed389c2fa89db56
AlineEspindola <aline.ae@aluno.ifsc.edu.br>
2024-02-22 23:16:38 -0300

    feat: Adiciona função que pega o valor do select

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 import './Select.css';
 class Select extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      select: {
+        language: ''
+      }
+    }
+
+    this.getLanguage = this.getLanguage.bind(this);
+  }
+
+  getLanguage(e) {
+    const select = { ...this.state.select };
+    select.language = e.target.value;
+    this.setState({ select });
+  }
+
   render() {
     return(
       <div>
-        <select>
-          <option>Normal</option>
-          <option>Invertido</option>
+        <select onChange={this.getLanguage} value={this.state.language} name="select">
+          <option value="normal">Normal</option>
+          <option value="inverted">Invertido</option>
         </select>
       </div>
     )
